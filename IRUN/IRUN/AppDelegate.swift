a27aa13a1b2546fc4a360c16bfe1fe26e27cac06
Dmytro Lutsyk <dmytro.lutsyk@orange.com>
2021-05-25 22:09:59 +0200

    first commit

---
+++ IRUN/IRUN/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  IRUN
+//
+//  Created by Dmytro LUTSYK on 25/05/2021.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UINavigationController(rootViewController: HomeViewController())
+        window.makeKeyAndVisible()
+        self.window = window
+
+        return true
+    }
+
+}
+
+
+
